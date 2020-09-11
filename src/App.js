3cb136dd9441a5d93dfedc4540d7a8ee8b7eb6d1
ramabrahmam <ramabrahmam.chowdavarapu@gmail.com>
2020-09-11 15:29:39 +0530

    Antd Modal integrated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
+import { Button, Modal} from 'antd';
+import styles from './style.module.scss';
 
 class App extends Component {
+  info() {
+    Modal.info({
+      title: 'This is a notification message',
+      content: (
+        <div>
+          <p>some messages...some messages...</p>
+          <p>some messages...some messages...</p>
+        </div>
+      ),
+      onOk() {},
+    });
+  }
+  
+  success() {
+    Modal.success({
+      content: 'some messages...some messages...',
+    });
+  }
+  
+  error() {
+    Modal.error({
+      title: 'This is an error message',
+      content: 'some messages...some messages...',
+    });
+  }
+  
+  warning() {
+    Modal.warning({
+      title: 'This is a warning message',
+      content: 'some messages...some messages...',
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>
-          Welcome React Antd Project Template
+          Welcome To Antd Modal Demo
         </h1>
+        <div className={styles.buttonContainer}>
+          <Button onClick={this.info}>Info</Button>
+          <Button onClick={this.success}>Success</Button>
+          <Button onClick={this.error}>Error</Button>
+          <Button onClick={this.warning}>Warning</Button>
+        </div>
       </div>
     );
   }
